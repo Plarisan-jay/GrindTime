4800881799d7fe0daafbbe9b9df9d57f5f1cf70b
kyleababan <marionababan60@gmail.com>
2025-08-27 15:34:28 +0700

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcpWjkaFBBagS5jU21dN6dm7u6jORY4_8",
+  authDomain: "grindtime-d6163.firebaseapp.com",
+  projectId: "grindtime-d6163",
+  storageBucket: "grindtime-d6163.firebasestorage.app",
+  messagingSenderId: "69884843905",
+  appId: "1:69884843905:web:d75ed78bc18e4bc1c8fa80"
+};
+
+
+const app = initializeApp(firebaseConfig);
